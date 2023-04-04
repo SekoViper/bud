67e7b02d879061fd7391fc7bbf0f1a7909cd5489
SekoViper <damehlivingstone1@gmail.com>
2023-04-04 16:27:26 +0000

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_164309) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_123505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_164309) do
   create_table "purchases", force: :cascade do |t|
     t.string "name"
     t.decimal "amount"
-    t.string "receiver"
+    t.string "reciever"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
