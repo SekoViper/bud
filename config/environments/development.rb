f4bc06242be4e6f10aac196e40cc52959cf6395b
SekoViper <damehlivingstone1@gmail.com>
2023-04-06 09:35:25 +0000

    Remove development email confirmation setup.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,14 +70,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :user_name => '79b7054d9fdfa7',
-  :password => '122137ca074f37',
-  :address => 'sandbox.smtp.mailtrap.io',
-  :domain => 'sandbox.smtp.mailtrap.io',
-  :port => '2525',
-  :authentication => :cram_md5
-}
 end
